df2949a4c2b74c3a2e84d1698326a82a406f55aa
sbulatao <114209345+sbulatao@users.noreply.github.com>
2022-11-16 16:18:03 -0800

    manual changes

--- grade.sh
+++ grade.sh
@@ -1,9 +1,34 @@
 # Create your grading script here
 
+set -e
+
+#CP = ''.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
 rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission 
 
+# Is this the correct file submitted
+if [ -f "list-methods-lab3.txt" ]
 
+then
+# if the file exist, this prints out
+echo "File is CORRECT"
 
+else
+# if the FILE does NOT exist, this prints out
+echo "File is NOT CORRECT"
+
+fi
+
+cp TestListExamples.java student-submission/
+
+cd student-submission/
+
+echo "In Student Submission"
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+echo "Compiled"
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
